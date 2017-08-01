eda2b42d623af85c31d7b422800feafad800bf00
Apoorv Suri <apoorvsuri2012@gmail.com>
2017-08-01 20:31:22 +0530

    Initial Commit

---
+++ JSONQuery/JSONQuery.h
@@ -0,0 +1,19 @@
+//
+//  JSONQuery.h
+//  JSONQuery
+//
+//  Created by Apoorv Suri on 01/08/17.
+//  Copyright © 2017 ASCorp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONQuery.
+FOUNDATION_EXPORT double JSONQueryVersionNumber;
+
+//! Project version string for JSONQuery.
+FOUNDATION_EXPORT const unsigned char JSONQueryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONQuery/PublicHeader.h>
+
+
